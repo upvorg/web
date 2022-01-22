a0345e0f6f688cb7b1db717988ead12fce4be90f
CY <34017352+shiyiya@users.noreply.github.com>
2022-01-22 20:41:38 +0800

    Update index.tsx

--- packages/coming/src/components/header/index.tsx
+++ packages/coming/src/components/header/index.tsx
@@ -4,16 +4,22 @@ import './index.scss'
 
 export default function Header() {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
+    <nav className="navbar " role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link href="/">
           <a className="navbar-item">
             <h1 className="logo">UPV</h1>
           </a>
         </Link>
+        
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
+      <div className="navbar-menu">
         <div className="navbar-start">
           <Link className="navbar-item" href="/">
             首页
