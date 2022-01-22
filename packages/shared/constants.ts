08596061029cb40df8f8858dbda4db290ee0d6fd
CY <34017352+shiyiya@users.noreply.github.com>
2022-01-23 00:26:30 +0800

    Update constants.ts

--- packages/shared/constants.ts
+++ packages/shared/constants.ts
@@ -1,6 +1,6 @@
 import http from './http'
 
-export const apiPerfix = !__DEV__ ? `http://127.0.0.1:8080` : '//api.upv.life'
+export const apiPerfix = __DEV__ ? `http://127.0.0.1:8080` : '//api.upv.life'
 export const storgePrefix = __DEV__ ? `http://127.0.0.1:8080` : '//storge.upv.life'
 
 export const axios = http.create<UPV.R.Response>(apiPerfix)
