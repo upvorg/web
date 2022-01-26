fef67c72eddae7c5a7b9dff0a35737ca966878a1
shiyiya <shiyiya.11@gmail.com>
2022-01-26 12:09:59 +0000

    .

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,4 @@
 // import { handlePush, handleNotificationClick, handleClientMessage } from './push-notification'
-import { respondWithCache } from './asset-cache'
 
 // declare const self: ServiceWorkerGlobalScope
 const ASSET_CACHE_PATTERN = /.+\.[0-9a-f]{8}\..+(js|css|woff2?|svg|png|jpg|jpeg|json|wasm)$/
@@ -46,3 +45,54 @@ self.addEventListener('fetch', (e /*: FetchEvent*/) => {
 // self.addEventListener('push', handlePush)
 // self.addEventListener('notificationclick', handleNotificationClick)
 // self.addEventListener('message', handleClientMessage)
+
+const ASSET_CACHE_NAME = 'asset-cache'
+
+// declare const self: ServiceWorkerGlobalScope
+
+// An attempt to fix freezing UI on iOS
+const CACHE_TIMEOUT = 3000
+
+async function respondWithCache(e /*: FetchEvent*/) {
+  const cacheResult = await withTimeout(async () => {
+    const cache = await self.caches.open(ASSET_CACHE_NAME)
+    const cached = await cache.match(e.request)
+
+    return { cache, cached }
+  }, CACHE_TIMEOUT)
+
+  const { cache, cached } = cacheResult || {}
+
+  if (cache && cached) {
+    if (cached.ok) {
+      return cached
+    } else {
+      await cache.delete(e.request)
+    }
+  }
+
+  const remote = await fetch(e.request)
+
+  if (remote.ok && cache) {
+    cache.put(e.request, remote.clone())
+  }
+
+  return remote
+}
+
+async function withTimeout(cb /*: () => Promise<T>*/, timeout /*: number*/) {
+  try {
+    return await Promise.race([
+      pause(timeout).then(() => Promise.reject(new Error('TIMEOUT'))),
+      cb()
+    ])
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err)
+    return undefined
+  }
+}
+
+function clearAssetCache() {
+  return self.caches.delete(ASSET_CACHE_NAME)
+}
