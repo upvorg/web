5045f11635e73dd7ce1cb7f8b1a93005ec8c7ff0
shiyiya <shiyiya.11@gmail.com>
2022-03-14 14:17:17 +0800

    .

--- packages/index/src/pages/about/index.tsx
+++ packages/index/src/pages/about/index.tsx
@@ -4,7 +4,7 @@ import Markdown from '../../components/markdown'
 const content = `
 ## 关于
 
-源码地址： https://github.com/upvorg is licensed GPL-3.0 。
+源码地址： [https://github.com/upvorg](https://github.com/upvorg) is licensed GPL-3.0 by upvorg .
 
 `
 
